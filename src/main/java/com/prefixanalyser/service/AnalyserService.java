8b10a70c83b1b8653bcce4a270cd5a457bc942cc
ertugruluy <ertugrul.uysal@argela.com.tr>
2021-05-02 19:09:48 +0300

    Mapping type changed

--- src/main/java/com/prefixanalyser/service/AnalyserService.java
+++ src/main/java/com/prefixanalyser/service/AnalyserService.java
@@ -3,16 +3,13 @@ package com.prefixanalyser.service;
 import com.prefixanalyser.Analyser;
 import com.prefixanalyser.NumberInfo;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/number")
 public class AnalyserService {
 	
-	@GetMapping(path = "/analyze")
+	@PostMapping(path = "/analyze")
 	public ResponseEntity<?> getInfo(@RequestBody String number) {
 		NumberInfo numberInfo = null;
 		try {
